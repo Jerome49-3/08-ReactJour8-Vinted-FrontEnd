6b40a43a23c5e72060ad3efa061fb108a11ae202
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-18 15:55:57 +0200

    setToken

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           <Route path="/" element={<Home search={search} />} />
           <Route path="/offer/:id" element={<Offer />} />
           <Route path="/signup" element={<SignUp icon1="eye" icon2="eye-slash" setToken={setToken} />} />
-          <Route path="/login" element={<SignIn setTok={setToken} />} />
+          <Route path="/login" element={<SignIn setToken={setToken} />} />
           <Route path="/publish" element={<Publish />} />
         </Routes>
         {show && <SignIn show={show} setShow={setShow} />}
