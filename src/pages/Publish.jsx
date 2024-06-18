584e62c7ab8bf353cc3037ffdb34fdcefc9bc5cc
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-18 14:33:56 +0200

    modif url et response dans login

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -1,45 +1,81 @@
-import Input from '../components/Input'
 import { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
 
-const SealIt = () => {
+//components
+import TextArea from '../components/TextArea';
+import Input from '../components/Input'
+
+const Publish = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState("0");
   const [brand, setBrand] = useState("");
   const [size, setSize] = useState("");
   const [condition, setCondition] = useState("");
   const [color, setColor] = useState("");
   const [city, setCity] = useState("");
   const [pictures, setPictures] = useState(null);
+  const navigate = useNavigate();
 
   const handleChangeFile = (e) => {
     setPictures(e.target.file)
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password !== confirmPassword) {
-      alert("Vos mots de passe ne sont pas identiques")
+    try {
+      const formData = new FormData();
+      formData.append('pictures', pictures);
+      formData.append('title', title);
+      formData.append('description', description);
+      formData.append('price', price);
+      formData.append('brand', brand);
+      formData.append('size', size);
+      formData.append('condition', condition);
+      formData.append('color', color);
+      formData.append('city', city);
+
+
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+        {
+          formData,
+        },
+        params,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+            "content-type": "multipart/form-data"
+          }
+        }
+      );
+      if (response) {
+        navigate(`/offer/${response.data._id}`);
+      }
+    } catch (error) {
+      console.log('error', error.response)
     }
   }
 
-  return (
+  return token ? (
     <div className='boxForm boxFormCenter'>
       <form onSubmit={handleSubmit}>
+        <input label="+ ajoute une photo !" htmlFor='pictures' type="file" id='pictures' name='pictures' onChange={() => {
+          handleChangeFile
+        }} multiple />
+        {pictures && <img src={pictures} alt="Image" />}
         <Input value={title} id="title" type="text" placeholder="nom du produit" setState={setTitle} />
-        <Input value={description} id="description" type="text" placeholder="Description" setState={setDescription} />
-        <Input value={price} id="price" type="text" placeholder="Prix" setState={setPrice} />
+        <TextArea name="description" value={description} id="description" type="text" placeholder="Description" setState={setDescription} />
+        <Input value={price} id="price" type="number" placeholder="Prix" setState={setPrice} />
         <Input value={brand} id="brand" type="text" placeholder="Marque" setState={setBrand} />
         <Input value={size} id="size" type="text" placeholder="Taille" setState={setSize} />
         <Input value={condition} id="condition" type="text" placeholder="Condition" setState={setCondition} />
         <Input value={color} id="color" type="text" placeholder="Couleur" setState={setColor} />
         <Input value={city} id="city" type="text" placeholder="Emplacement" setState={setCity} />
-        <input type="file" id='pictures' name='pictures' onChange={() => {
-          handleChangeFile
-        }} multiple />
         <Input type="submit" value="poster votre annonce" />
       </form>
     </div>
   )
+    : <Navigate to="/login" />
 }
 
-export default SealIt
+export default Publish
