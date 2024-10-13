98e1e19100f598ce1a91633dd7aa4d33e33d5d6d
Jerome Bobin <jeromebobin49.3@gmail.com>
2024-10-13 15:43:47 +0200

    add _redirects

--- src/pages/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail.jsx
@@ -16,10 +16,12 @@ const ConfirmEmail = () => {
     const loadData = async () => {
       try {
         const response = await axios.get(`https://site--vintedbackend--s4qnmrl7fg46.code.run/user/confirmEmail/${code}`);
-        const token = response.data;
-        saveUser(token);
-        alert(response.data.messsage);
-        navigate("/publish");
+        if (response) {
+          const token = response.data;
+          saveUser(token);
+          alert(response.data.messsage);
+          navigate("/publish");
+        }
       } catch (error) {
         setErrorMessage(error.response.data.message);
       }
