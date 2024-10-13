269db0a3605690600b0a919e4d2d16805694b2d7
Jerome Bobin <jeromebobin49.3@gmail.com>
2024-10-13 14:50:40 +0200

    change confirmEmail

--- src/pages/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail.jsx
@@ -1,49 +1,26 @@
 import axios from "axios";
-import { useState } from "react";
-import { useUser } from '../context/lib/userFunc';
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
-//components
-import Input from '../components/Input';
+import { useUser } from '../context/lib/userFunc';
 import Loading from "../components/Loading";
 
+export const ConfirmEmail = () => {
+  const { code } = useParams();
+  const { saveUser, setErrorMessage } = useUser();
 
-const ConfirmEmail = () => {
-  const { saveUser, errorMessage, setErrorMessage } = useUser();
-  let [code, setCode] = useState(null);
-  code = useParams();
-  console.log('code on /confirmEmail:', code);
-  const navigate = useNavigate();
-
-  const handleConfirmEmail = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("code", code);
-    try {
-      const response = axios.post(`https://site--vintedbackend--s4qnmrl7fg46.code.run/confirmEmail/${code}`,
-        formData,
-      );
-      if (response) {
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await axios.get(`https://site--vintedbackend--s4qnmrl7fg46.code.run/user/confirmEmail/${code}`);
         const token = response.data;
         saveUser(token);
-        alert('Votre mail à bien été confirmé');
-        navigate("/publish");
+      } catch (error) {
+        setErrorMessage(error.response.data.message);
       }
-    } catch (error) {
-      setErrorMessage(error.response.data.message);
-    }
-  }
+    };
 
-  return (
-    <div>
-      <form onSubmit={handleConfirmEmail}>
-        <Input value={code} type='text' setState={setCode} />
-        <Input type='submit' value='envoyer le code' />
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      </form>
-    </div>
-  )
-}
+    loadData();
+  }, [code]);
 
-export default ConfirmEmail
+  return <div><Loading /></div>;
+};
