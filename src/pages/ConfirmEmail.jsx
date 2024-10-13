3ab895840e7380af9ce5b43641adf96ad3799290
Jerome Bobin <jeromebobin49.3@gmail.com>
2024-10-13 14:53:41 +0200

    change confirmEmail

--- src/pages/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail.jsx
@@ -2,11 +2,15 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useUser } from '../context/lib/userFunc';
+import { useNavigate } from 'react-router-dom';
+
+//components
 import Loading from "../components/Loading";
 
 export const ConfirmEmail = () => {
   const { code } = useParams();
   const { saveUser, setErrorMessage } = useUser();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loadData = async () => {
@@ -14,6 +18,7 @@ export const ConfirmEmail = () => {
         const response = await axios.get(`https://site--vintedbackend--s4qnmrl7fg46.code.run/user/confirmEmail/${code}`);
         const token = response.data;
         saveUser(token);
+        navigate("/publish");
       } catch (error) {
         setErrorMessage(error.response.data.message);
       }
