71d206e45f1e55c835dc7c892fefdfcdba48f29d
Jerome Bobin <jeromebobin49.3@gmail.com>
2024-10-14 15:29:10 +0200

    return to past

--- src/pages/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail.jsx
@@ -1,38 +1,37 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { useUser } from '../context/lib/userFunc';
 import { useNavigate } from 'react-router-dom';
 
 //components
-import Loading from "../components/Loading";
+import Input from '../components/Input';
 
 const ConfirmEmail = () => {
-  const { code } = useParams();
+  const [code, setCode] = useState(null);
   console.log('code  on /confirmEmail:', code);
   const { saveUser, setErrorMessage } = useUser();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const response = await axios.get(`https://site--vintedbackend--s4qnmrl7fg46.code.run/user/confirmEmail/${code}`);
-        // const response = await axios.get(`http/localhost:3000/user/confirmEmail/${id}`);
-        if (response) {
-          const token = response.data;
-          saveUser(token);
-          alert(response.data.messsage);
-          navigate("/publish");
-        }
-      } catch (error) {
-        setErrorMessage(error.response.data.message);
+  const handleConfirmEmail = async () => {
+    try {
+      const response = await axios.get(`https://site--vintedbackend--s4qnmrl7fg46.code.run/user/confirmEmail/${code}`);
+      // const response = await axios.get(`http/localhost:3000/user/confirmEmail/${id}`);
+      if (response) {
+        const token = response.data;
+        saveUser(token);
+        alert(response.data.messsage);
+        navigate("/publish");
       }
-    };
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
 
-    loadData();
-  }, []);
   return (
-    <Loading />
+    <form onSubmit={handleConfirmEmail} className="boxForm boxFormCenter">
+      <Input value={code} type='text' setState={setCode} />
+      <Input type='submit' value='envoyer le code' />
+    </form>
   )
 }
 
