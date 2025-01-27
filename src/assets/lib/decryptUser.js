420598fac803ab593f256d7be1e5bd0550750fe9
jerome <jeromebobin49.3@gmail.com>
2025-01-27 23:10:06 +0100

    console.log token

--- src/assets/lib/decryptUser.js
+++ src/assets/lib/decryptUser.js
@@ -2,12 +2,12 @@ import { jwtDecode } from "jwt-decode";
 
 const decryptUser = (token) => {
   if (token && typeof token === "string") {
-    // console.log("token in decryptUser:", token);
-    // console.log("typeof token in decryptUser:", typeof token);
+    console.log("token in decryptUser:", token);
+    console.log("typeof token in decryptUser:", typeof token);
     try {
       const decoded = jwtDecode(token);
-      // console.log("decoded in decryptUser:", decoded);
-      // console.log("typeof decoded in decryptUser:", typeof decoded);
+      console.log("decoded in decryptUser:", decoded);
+      console.log("typeof decoded in decryptUser:", typeof decoded);
       return decoded;
     } catch (error) {
       console.error("Error decrypting user:", error);
