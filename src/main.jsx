d21e4bf2e82de359d96e17224327d5c0afa258ed
jerome <jeromebobin49.3@gmail.com>
2025-01-28 02:23:26 +0100

    add sentryLogs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/css/index.css';
-import App from './App.jsx';
-import ThemeProvider from './context/ThemeProvider.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import "./assets/css/index.css";
+import App from "./App.jsx";
+import * as dotenv from "dotenv";
+import ThemeProvider from "./context/ThemeProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Sentry Init
+Sentry.init({
+  dsn: import.meta.env.VITE_REACT_APP_SENTRY_DSN,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [
+    "https://vintaid.netlify.app/",
+    /^https:\/\/yourserver\.io\/api/,
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider value="light">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+    <Sentry.ErrorBoundary fallback={<h1>Something went wrong</h1>}>
+      <ThemeProvider value="light">
+        <App />
+      </ThemeProvider>
+    </Sentry.ErrorBoundary>
+  </React.StrictMode>
+);
